c284da1b5fb13c269be47e269e21d1bb2eadbb60
YYYYYYYYYYYYYYYY <314564825@qq.com>
2016-10-21 13:16:16 +0800

    java的异常捕获验证

---
+++ Java异常捕获/ExceptionClass1.java
@@ -0,0 +1,18 @@
+﻿package john.Exception;
+
+public class ExceptionClass1 {
+	public static void main(String[] args) {
+		System.out.println("进行一个除数为零的除法运算来验证java异常捕获机制");
+		int i = 100;
+		int j = 0;
+		try {
+			int temp = i / j;
+			System.out.println("计算结果为："+temp);
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("出现异常："+e);
+		}finally{
+			System.out.println("是否出现异常都会执行该语句");
+		}
+	}
+}
---
+++ Java异常捕获/ExceptionClass2.java
@@ -0,0 +1,30 @@
+﻿package john.Exception;
+
+class Math{
+	public void div(int i,int j)throws Exception{
+		System.out.println("开始计算：");
+		int temp = 0;
+		try {
+			temp = i/j;
+			System.out.println("结果为："+temp);
+		} catch (Exception e) {
+			// TODO: handle exception
+			throw e;
+		}finally {
+			System.out.println("是否出现异常都会执行此语句");
+			System.out.println("计算结束");
+		}
+	}
+}
+
+public class ExceptionClass2 {
+	public static void main(String[] args) {
+		Math math = new Math();
+		try {
+			math.div(100, 0);
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("捕获到异常："+e);
+		}
+	}
+}
